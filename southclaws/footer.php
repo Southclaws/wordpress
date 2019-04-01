84808059ec5caee84545191423e8f953b5938f22
Barnaby Keene <hello@southcla.ws>
2019-04-01 18:48:21 +0100

    formatted all php files

--- southclaws/footer.php
+++ southclaws/footer.php
@@ -1,10 +1,13 @@
 </div>
 <footer id="footer">
-<div id="copyright">
-&copy; <?php echo esc_html( date_i18n( __( 'Y', 'blankslate' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-</div>
+    <div id="copyright">
+        &copy; <?php echo esc_html(
+                    date_i18n(__('Y', 'blankslate'))
+                ); ?> <?php echo esc_html(get_bloginfo('name')); ?>
+    </div>
 </footer>
 </div>
 <?php wp_footer(); ?>
 </body>
-</html>
+
+</html> 
