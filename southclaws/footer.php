baacf9dd53d5588a77dbdd60cca28aeb5e209173
Barnaby Keene <hello@southcla.ws>
2019-04-02 18:16:04 +0100

    footer

--- southclaws/footer.php
+++ southclaws/footer.php
@@ -1,10 +1,9 @@
 </div>
-<footer id="footer">
-    <div id="copyright">
-        &copy; <?php echo esc_html(
-                    date_i18n(__('Y', 'blankslate'))
-                ); ?> <?php echo esc_html(get_bloginfo('name')); ?>
-    </div>
+<hr />
+<footer>
+    <p>
+        <?php echo esc_html(date_i18n('Y')); ?> &copy; Barnaby "Southclaws" Keene <span>All Rights Reserved</span>
+    </p>
 </footer>
 </div>
 <?php wp_footer(); ?>
